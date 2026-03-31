5ad885309c323b085e00efcc8ca5fc7befacd995
682023025-Agil <682023025@student.uksw.edu>
2026-03-31 23:19:35 +0700

    Include dist and lib for deployment

---
+++ lib/CustomUI/PasswordFieldCustom.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CustomUI;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
+import javax.swing.JPasswordField;
+
+public class PasswordFieldCustom extends JPasswordField {
+
+    private int radius = 15;
+
+    public PasswordFieldCustom() {
+        setOpaque(false);
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fill(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), radius, radius));
+        super.paintComponent(g);
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+    Graphics2D g2 = (Graphics2D) g.create();
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    
+    // Warna abu-abu soft
+    g2.setColor(new java.awt.Color(180, 180, 180));
+    
+    // Garis tipis
+    g2.setStroke(new java.awt.BasicStroke(1f));
+    
+    g2.draw(new RoundRectangle2D.Double(0, 0, getWidth() - 1, getHeight() - 1, radius, radius));
+    g2.dispose();
+}
+
+    
+
+    @Override
+    public boolean contains(int x, int y) {
+        Shape shape = new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), radius, radius);
+        return shape.contains(x, y);
+    }
+
+    public void setHint(String password) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
